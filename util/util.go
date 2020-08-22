5dd1d6ad989abbf4228a43c4a14f9af8222d8f9b
sliveryou <1215299941@qq.com>
2020-08-22 21:32:11 +0800

    Add convertDataType util function

--- util/util.go
+++ util/util.go
@@ -5,6 +5,88 @@ import (
 	"unicode"
 )
 
+// convertDataType converts the mysql data type to golang data type.
+func convertDataType(ci *ColumnInfo, cc *CMDConfig) string {
+	switch ci.DataType {
+	case "tinyint", "smallint", "mediumint":
+		if ci.IsNullable {
+			if cc.EnableGureguNull {
+				return gureguNullInt
+			} else if cc.EnableSqlNull {
+				return sqlNullInt32
+			}
+		}
+		if ci.IsUnsigned {
+			return goUint32
+		}
+		return goInt32
+	case "int", "integer":
+		if ci.IsNullable {
+			if cc.EnableGureguNull {
+				return gureguNullInt
+			} else if cc.EnableSqlNull {
+				return sqlNullInt64
+			}
+		}
+		if ci.IsUnsigned {
+			return goUint
+		}
+		return goInt
+	case "bigint":
+		if ci.IsNullable {
+			if cc.EnableGureguNull {
+				return gureguNullInt
+			} else if cc.EnableSqlNull {
+				return sqlNullInt64
+			}
+		}
+		if ci.IsUnsigned {
+			return goUint64
+		}
+		return goInt64
+	case "json", "enum", "set", "char", "varchar", "tinytext", "text", "mediumtext", "longtext":
+		if ci.IsNullable {
+			if cc.EnableGureguNull {
+				return gureguNullString
+			} else if cc.EnableSqlNull {
+				return sqlNullString
+			}
+		}
+		return goString
+	case "year", "date", "datetime", "time", "timestamp":
+		if ci.IsNullable {
+			if cc.EnableGureguNull {
+				return gureguNullTime
+			} else if cc.EnableSqlNull {
+				return sqlNullTime
+			}
+		}
+		return goTime
+	case "float":
+		if ci.IsNullable {
+			if cc.EnableGureguNull {
+				return gureguNullFloat
+			} else if cc.EnableSqlNull {
+				return sqlNullFloat64
+			}
+		}
+		return goFloat32
+	case "double", "real", "decimal", "numeric":
+		if ci.IsNullable {
+			if cc.EnableGureguNull {
+				return gureguNullFloat
+			} else if cc.EnableSqlNull {
+				return sqlNullFloat64
+			}
+		}
+		return goFloat64
+	case "bit", "binary", "varbinary", "tinyblob", "blob", "mediumblob", "longblob":
+		return goBytes
+	default:
+		return "unknown"
+	}
+}
+
 // convertName converts the name to camel case name.
 func convertName(name string) string {
 	if name == "" {
--- util/util_test.go
+++ util/util_test.go
@@ -4,6 +4,35 @@ import (
 	"testing"
 )
 
+func TestConvertDataType(t *testing.T) {
+	cases := []struct {
+		ci          ColumnInfo
+		cc          CMDConfig
+		expectation string
+	}{
+		{ColumnInfo{DataType: "tinyint", IsNullable: true, IsUnsigned: false},
+			CMDConfig{EnableSqlNull: false, EnableGureguNull: false}, "int32",
+		},
+		{ColumnInfo{DataType: "tinyint", IsNullable: false, IsUnsigned: true},
+			CMDConfig{EnableSqlNull: false, EnableGureguNull: false}, "uint32",
+		},
+		{ColumnInfo{DataType: "tinyint", IsNullable: true, IsUnsigned: false},
+			CMDConfig{EnableSqlNull: true, EnableGureguNull: false}, "sql.NullInt32",
+		},
+		{ColumnInfo{DataType: "tinyint", IsNullable: true, IsUnsigned: true},
+			CMDConfig{EnableSqlNull: true, EnableGureguNull: true}, "null.Int",
+		},
+	}
+
+	for _, c := range cases {
+		output := convertDataType(&c.ci, &c.cc)
+		if output != c.expectation {
+			t.Errorf("convertDataType failed, expectation:%s, output:%s",
+				c.expectation, output)
+		}
+	}
+}
+
 func TestConvertName(t *testing.T) {
 	cases := []struct {
 		input       string
@@ -21,7 +50,8 @@ func TestConvertName(t *testing.T) {
 	for _, c := range cases {
 		output := convertName(c.input)
 		if output != c.expectation {
-			t.Errorf("convertName failed, input:%s, expectation:%s, output:%s", c.input, c.expectation, output)
+			t.Errorf("convertName failed, input:%s, expectation:%s, output:%s",
+				c.input, c.expectation, output)
 		}
 	}
 }

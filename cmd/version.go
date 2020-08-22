feffa4ee9fb5eeea0025212048740b432be84488
sliveryou <1215299941@qq.com>
2020-08-22 20:16:06 +0800

    Add version subcommand

---
+++ cmd/version.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	projectName    = "Grom"
+	projectVersion = "1.0.0"
+	goVersion      = ""
+	gitCommit      = ""
+	buildTime      = ""
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show the grom version information",
+	Long:  "Show the grom version information, such as project name, project version, go version, git commit id, build time, etc",
+	Run:   versionFunc,
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+func versionFunc(cmd *cobra.Command, args []string) {
+	fmt.Printf("Cmd Tool: %s\n", projectName)
+	fmt.Printf(" Version: %s\n", projectVersion)
+	fmt.Printf(" Go Version: %s\n", goVersion)
+	fmt.Printf(" Git Commit: %s\n", gitCommit)
+	fmt.Printf(" Build Time: %s\n", buildTime)
+}

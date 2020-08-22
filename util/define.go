cdc5b3f9e4f1d79344420dfb0be5a9cabbee331a
sliveryou <1215299941@qq.com>
2020-08-22 20:37:40 +0800

    Add some structure definitions

--- util/define.go
+++ util/define.go
@@ -5,6 +5,39 @@ const (
 	MySQLDriverName = "mysql"
 )
 
+const (
+	indexUnique = 0
+	indexNormal = 1
+)
+
+const (
+	gureguNullString = "null.String"
+	gureguNullInt    = "null.Int"
+	gureguNullFloat  = "null.Float"
+	gureguNullBool   = "null.Bool"
+	gureguNullTime   = "null.Time"
+
+	sqlNullString  = "sql.NullString"
+	sqlNullInt32   = "sql.NullInt32"
+	sqlNullInt64   = "sql.NullInt64"
+	sqlNullFloat64 = "sql.NullFloat64"
+	sqlNullBool    = "sql.NullBool"
+	sqlNullTime    = "sql.NullTime"
+
+	goString  = "string"
+	goBytes   = "[]byte"
+	goInt     = "int"
+	goUint    = "uint"
+	goInt32   = "int32"
+	goUint32  = "uint32"
+	goInt64   = "int64"
+	goUint64  = "uint64"
+	goFloat32 = "float32"
+	goFloat64 = "float64"
+	goBool    = "bool"
+	goTime    = "time.Time"
+)
+
 // CMDConfig represents the config of the running grom command line.
 type CMDConfig struct {
 	DBConfig
@@ -29,4 +62,40 @@ type DBConfig struct {
 	Password string `json:"password"`
 	Database string `json:"database"`
 	Table    string `json:"table"`
+}
+
+// StructField represents the field of the generated model structure.
+type StructField struct {
+	Name    string
+	Type    string
+	Tag     string
+	Comment string
+}
+
+// ColumnInfo represents the information of the column.
+type ColumnInfo struct {
+	Name            string       `mysql:"COLUMN_NAME"`
+	DataType        string       `mysql:"DATA_TYPE"`
+	Type            string       `mysql:"COLUMN_TYPE"`
+	Default         string       `mysql:"COLUMN_DEFAULT"`
+	Comment         string       `mysql:"COLUMN_COMMENT"`
+	Length          int64        `mysql:"CHARACTER_MAXIMUM_LENGTH"`
+	Precision       int64        `mysql:"NUMERIC_PRECISION"`
+	Scale           int64        `mysql:"NUMERIC_SCALE"`
+	Position        int          `mysql:"ORDINAL_POSITION"`
+	IsPrimaryKey    bool         `mysql:"COLUMN_KEY"`
+	IsAutoIncrement bool         `mysql:"EXTRA"`
+	IsUnsigned      bool         `mysql:"COLUMN_TYPE"`
+	IsNullable      bool         `mysql:"IS_NULLABLE"`
+	Indexes         []*IndexInfo `mysql:"-"`
+	UniqueIndexes   []*IndexInfo `mysql:"-"`
+}
+
+// IndexInfo represents the information of the index.
+type IndexInfo struct {
+	Name       string `mysql:"INDEX_NAME"`
+	ColumnName string `mysql:"COLUMN_NAME"`
+	Comment    string `mysql:"INDEX_COMMENT"`
+	Sequence   int    `mysql:"SEQ_IN_INDEX"`
+	IsUnique   bool   `mysql:"NON_UNIQUE"`
 }
